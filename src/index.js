bea698123632459066b0e26ecb9ce491470cfc47
Luka <luka.luka.luka93@gmail.com>
2024-05-28 17:25:32 -0700

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import NameTag from "./NameTag.js";
+import { names } from "./data.js";
+
+import "./styles.css";
+const renderNameTag = (name) => <NameTag name={name.name} key={name.id} />;
+const App = () => {
+  const NameTagElements = names.map(renderNameTag);
+  return (
+    <div className="App">
+      <h1>Name Tag Generator</h1>
+      {NameTagElements}
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);

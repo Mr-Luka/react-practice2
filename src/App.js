5b5f53793a66cc4d272ebb63f7cbbd7439f36f4b
Luka <luka.luka.luka93@gmail.com>
2024-05-28 17:47:35 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -5,4 +5,18 @@ class App extends Component {
   state = {
     names: ["Agumon", "Gabumon", "Patamon"]
   }
+  removeName = (clickedIndex) => {
+    const filterCallback = (__, index) => index !== clickedIndex;
+    const newNames = this.state.names.map(filterCallback);
+    this.setState({names: newNames});
+  }
+  render () {
+    return (
+      <div className="App">
+        <h1>Digimon</h1>
+        <NameTagList names={this.state.names} removeNames = {this.removeName}/>
+      </div>
+    )
+  }
 }
+export default App;

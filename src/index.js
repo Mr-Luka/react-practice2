e400a5163180fce7ce1ea51e8e6a016a1f7f9d25
Luka <luka.luka.luka93@gmail.com>
2024-05-28 17:41:40 -0700

    index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import NameTag from "./NameTag.js";
-import { names } from "./data.js";
+import App from "./App.js"
+
 
 import "./styles.css";
-const renderNameTag = name => <NameTag name={name.name} key={name.id}/>
-const App = () => {
- const mapNames = names.map(renderNameTag);
-  return (
-    <div className="App">
-      <h1>Digimon</h1>
-    {mapNames}
-    </div>
-  );
-};
+
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
